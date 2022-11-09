aef48e7d21546253ee8dc48952656433f226fa39
andreasdelu <Andreasdelu@gmail.com>
2022-11-09 11:33:39 +0100

    Start and import fonts

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+
+function App() {
+	return (
+		<div id='mainContent'>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
+}
+
+export default App;
